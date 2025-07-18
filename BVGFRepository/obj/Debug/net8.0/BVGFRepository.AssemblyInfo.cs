f08bbbd0b34b973c22403b4de753300b807bcf69
rahul <rahulsinghalmumbai@gmail.com>
2025-07-18 11:40:22 +0530

    changes

--- BVGFRepository/obj/Debug/net8.0/BVGFRepository.AssemblyInfo.cs
+++ BVGFRepository/obj/Debug/net8.0/BVGFRepository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BVGFRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4d0a21b4066f368065c4b8ac96395b08b8120a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BVGFRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BVGFRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
